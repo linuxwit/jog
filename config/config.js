3b8e7c0a879eb16d69db06e25c3be4ef35f5e414
witwave <witwave@outlook.com>
2014-03-08 22:34:44 +0800

    db

--- config/config.js
+++ config/config.js
@@ -4,33 +4,12 @@ module.exports = {
 		db: 'mongodb://localhost/jog',
 		app: {
 			name: 'love jog'
-		},
-		facebook: {
-			clientID: "{{PLACEHOLDER}}",
-			clientSecret: "{{PLACEHOLDER}}",
-			callbackURL: "{{PLACEHOLDER}}"
-		},
-
-		google: {
-			clientID: "{{PLACEHOLDER}}",
-			clientSecret: "{{PLACEHOLDER}}",
-			callbackURL: "{{PLACEHOLDER}}"
 		}
 	},
   	production: {
-    	db: generate_mongo_url(),
+    	db: 'mongodb://localhost/jog',
 		app: {
-			name: 'mongodb://localhost/jog'
-		},
-		facebook: {
-			clientID: "",
-			clientSecret: "",
-			callbackURL: ""
-		},
-		google: {
-			clientID: '',
-			clientSecret: '',
-			callbackURL: ''
+			name: 'love jog'
 		}
  	}
 }

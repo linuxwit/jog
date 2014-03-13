05ddf72d637b157dc919143e23758d0db360892c
witwave <witwave@outlook.com>
2014-03-13 20:51:55 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var post=require('../models/post');
-
+var moment = require('moment');
 module.exports = function (app, passport) {
     app.get('/', function (req, res) {
+        moment.lang('zh-cn');
         var page=req.param("page")?parseInt(req.param("page")):0;
-        var query= post.find({}, {}, { skip: page*10, limit: 10 }).sort('posted -1');
+        var query= post.find({}, {}, { skip: page*10, limit: 10 }).sort({ posted: 'desc' });
          query.exec(function (err, docs) {
-            res.render('index',{posts:docs,page:page,qiniu_host:'http://lovejog.qiniudn.com'});
-        });
-
+            res.render('index',{
+                posts:docs,
+                moment:moment,
+                page:page,
+                qiniu_host:'http://lovejog.qiniudn.com'});
+        })
     })
 }

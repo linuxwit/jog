fc996ddc2d834df648473140dae8f7e420ab6ed5
witwave <witwave@outlook.com>
2014-03-17 23:30:59 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -59,7 +59,7 @@ module.exports = function (app, passport) {
 
     app.get('/logout', function (req, res) {
         req.logout();
-        res.redirect('/login');
+        res.redirect('/signin');
     });
 
     app.post("/login"

0e1d6459688b8230619aaf8321879c5559e0042c
witwave <witwave@outlook.com>
2014-03-21 21:26:41 +0800

    init

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "
 then
         export PATH=../../node/0.10.26/bin:/usr/local/bin:$PATH
         #forever start --sourceDir /home/sites/jog app.js >> /home/logs/log.txt 2>&1
-        supervisor node  /home/sites/jog app.js >> /home/logs/log.txt 2>&1
+        forever start -w --sourceDir /home/sites/jog app.js >>/home/logs/log.txt 2>&1
 fi
 
 # @reboot /path/to/starter.sh

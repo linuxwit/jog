232e90b013d4148d90b66970a5c159e6cd08e6d1
witwave <witwave@outlook.com>
2014-04-05 22:09:51 +0800

    header

--- middlewares/mail.js
+++ middlewares/mail.js
@@ -27,20 +27,7 @@ exports.send=function(subject,content,from,to){
             return;
         }
         console.log('send ok');
-        // response.statusHandler only applies to 'direct' transport
-        response.statusHandler.once("failed", function(data){
-            console.log(
-                "Permanently failed delivering message to %s with the following response: %s",
-                data.domain, data.response);
-        });
 
-        response.statusHandler.once("requeue", function(data){
-            console.log("Temporarily failed delivering message to %s", data.domain);
-        });
-
-        response.statusHandler.once("sent", function(data){
-            console.log("Message was accepted by %s", data.domain);
-        });
     });
 
 
